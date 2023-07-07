b685f9df8284fcea06edd2fd38ae61078bbade0b
tRQ0 <rq1087@gmail.com>
2023-07-07 17:28:58 +0530

    update dependancy

--- index.php
+++ index.php
@@ -4,33 +4,31 @@ use Goutte\Client;
 use Symfony\Component\HttpClient\HttpClient;
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/crawl.php';
 
+/**
+ * Misscelaneous variables
+ * 
+ * @var $client Gouttle\Client $client A crawler client instance
+ * @val $url string $url The url to crawl
+ * @var $inStockText stores $inStockText The exact text that is stored in #availability tag when a product is in stock
+ */
 $client = new Client(HttpClient::create(['verify_host' => false]));
-$text = [];
-$finalLinks = [];
-$url = 'https://codingcafe.website';
- 
-function crawlLink($url, $client){
-    
-    $crawler = $client->request('GET', $url);
-    $text =  $crawler->filter('h1, h4, h5, h6, span, h3, h2, p, a')->each(function ($node) {
-      return $node->text()."<br>";
-    });
-    $text = array_filter($text);
-    $text = implode('<br>', $text);
-    $finalLinks['url'] = $url;
-    $finalLinks['content'] = $text;
-    $finalLinks['words'] = str_word_count($text);
-    $links =  $crawler->filter('a')->each(function ($node) use($url) {
-        $link = $node->attr('href');
-        if(str_contains($link, $url)) {
-            if(str_ends_with($link, '/')) {
-                return substr($link, 0, strlen($link) - 1);
-            }
-            return $link;
-        }
-    });
-    $links = array_unique(array_filter($links));
+$url = 'https://www.amazon.in/dp/B072PRH59B/?coliid=I1J7S6167YE0ZW&colid=Z1348PFM3I4R&psc=0&ref_=list_c_wl_lv_ov_lig_dp_it';
+$inStockText = 'In stock';
+
+
+$baseURL = parse_url($url, PHP_URL_HOST);
+/** @var string $availabilityText Text contained in #availability */
+$availabilityText = implode('', crawl($client, $url, '#availability'));
+
+/** Fetching the price of the product */
+if (strcmp($availabilityText, $inStockText) == 0) {
+    $price = crawl($client, $url, '#corePriceDisplay_desktop_feature_div > div > .reinventPricePriceToPayMargin'); // '#corePriceDisplay_desktop_feature_div >  .reinventPricePriceToPayMargin');
+    $price = implode('', array_unique(array_filter(explode('₹', $price[0]))));
+    echo 'Availability: ' . $availabilityText . '<br>Price: ' . $price;
 }
-crawlLink($url, $client);
-print_r($finalLinks);
+
+echo 'Availability: '. $availabilityText;
+
+?>
